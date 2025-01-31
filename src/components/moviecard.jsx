e087bd208ba05b3c2974b9aebf49639c92046fe8
Misbah Khursheed <m.misbahkhursheed@gmail.com>
2025-01-31 19:31:22 +0530

    regular updates

--- src/components/moviecard.jsx
+++ src/components/moviecard.jsx
@@ -1,12 +1,24 @@
 import '../styles/movie.css'
 
 export default function MovieCard(props) { 
+    function like() {
+        alert('You liked this movie')
+    }
     return (
         <div className="movie-card">
-            <img src={props.poster} alt={props.title} />
-            <h3>{props.title}</h3>
-            <p>{props.year}</p>
-            <p>{props.genre}</p>
+            <div className='movie-poster' >
+                <img src={props.poster} alt={props.title} />
+                <div className='movie-overlay'>
+                    <button className='favourite-btn' onClick={like()}> ❤️ </button>
+                </div>
+            </div>
+            <div className='movie-info'>
+                <h3 className=''>{props.title}</h3>
+                <p>{props.year}</p>
+                <p>{props.genre}</p>
+            </div>
+            
         </div>
     )
 }
+
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,5 +1,5 @@
 import "../styles/App.css"
-import Navbar from '../components/Navbar'
+import Navbar from '../components/navbar'
 import MovieCard from '../components/moviecard' 
 
 export default function App() {
@@ -7,6 +7,7 @@ export default function App() {
   return (
     <>
       <Navbar />
+      <MovieCard title="The Shawshank Redemption" year="1994" genre="Drama" poster="https://images-na.ssl-images-amazon.com/images/I/51S9gFmJQ-L._AC_.jpg" />
     </>
   )
 }
